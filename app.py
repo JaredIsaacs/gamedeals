28842f028f04a83088ebdb763e4f548ac320b6a8
JaredIsaacs <jaredisaacs6@gmail.com>
2023-11-08 18:47:59 -0500

    feat: added a stylesheets

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    #app.run(debug=True, port=8080)
+    app.run(debug=True, port=8080)
     
-    upper_price = 15
-    response = requests.get(f"https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice={upper_price}&pageNumber=0").json()
-    with open("response.json", 'w') as outfile:
-        json.dump(response, outfile, indent=4)
+    #upper_price = 15
+    #response = requests.get(f"https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice={upper_price}&pageNumber=0").json()
+    #with open("response.json", 'w') as outfile:
+    #    json.dump(response, outfile, indent=4)
