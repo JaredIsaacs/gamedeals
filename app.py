de811263101ef4e907ce79b4b3c28bd27f8fbda8
JaredIsaacs <jaredisaacs6@gmail.com>
2023-11-15 13:12:18 -0500

    feat: can sort without submit button. Formatting

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from wtforms.validators import NumberRange
 
 #https://apidocs.cheapshark.com/#intro
 
-DEFAULT_MIN_PRICE = .01
+DEFAULT_MIN_PRICE = 0
 DEFAULT_MAX_PRICE = 60
 
 app = Flask(__name__)
@@ -19,11 +19,11 @@ class Menus(FlaskForm):
     name = StringField('Name')
     min_price = DecimalField('Minimum Price', validators=[NumberRange(min=DEFAULT_MIN_PRICE)], default=DEFAULT_MIN_PRICE)
     max_price = DecimalField('Maximum Price', default=DEFAULT_MAX_PRICE)
-    submit = SubmitField('Submit')
+    submit = SubmitField('Search!')
 
-def get_games(title: str = '', lower_price: float = DEFAULT_MIN_PRICE, upper_price: float = DEFAULT_MAX_PRICE) -> dict:
-    base_url = 'https://www.cheapshark.com/api/1.0/deals?storeID=1&lowerPrice={0}&upperPrice={1}&title={2}'
-    response = requests.get(base_url.format(lower_price, upper_price, title)).json()
+def get_games(title: str = '', lower_price: float = DEFAULT_MIN_PRICE, upper_price: float = DEFAULT_MAX_PRICE, page_number: int = 0) -> dict:
+    base_url = 'https://www.cheapshark.com/api/1.0/deals?storeID=1&lowerPrice={0}&upperPrice={1}&title={2}&pageSize=15&pageNumber={3}'
+    response = requests.get(base_url.format(lower_price, upper_price, title, page_number)).json()
     return response
 
 @app.route('/', methods=["GET", "POST"])
@@ -31,7 +31,7 @@ def index():
     form = Menus()
     
     if request.method == 'POST':
-        game_name = form.name.data.strip()
+        game_name = form.name.data
         min_price = form.min_price.data
         max_price = form.max_price.data
         games = get_games(lower_price=min_price, upper_price=max_price, title=game_name)
